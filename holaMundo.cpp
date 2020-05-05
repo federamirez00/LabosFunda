390e46e7dbffcef9a21c3503b51cce11ccbac3cc
federamirez00 <federicoramirez0050@gmail.com>
2020-05-05 16:42:46 -0600

    Se agregó el ejemplo

---
+++ holaMundo.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std; //permite usar cout de forma abreviada
+int main()
+{
+    /*string palabra1 = "Hola";
+    string palabra2 = "Mundo";
+    string concatenar = palabra1+" "+palabra2;*/
+
+    /*int numero1 = 5;
+    int numero2 = 10;
+
+    int suma = numero1 + numero2;
+    int resta = numero1 - numero2;
+    int multiplicacion = numero1 * numero2;
+    int division = numero2 / numero1;
+    int mod = numero2%numero1;
+
+    cout << "La suma es: " << suma << endl;
+    cout << "La resta es: " << resta << endl;
+    cout << "La multiplicacion es: " << multiplicacion << endl;
+    cout << "La division es: " << division << endl;
+    cout << "El modulo es: " << mod << endl;*/
+
+    /*int numero1;
+    int numero2;
+
+    cout<<"Ingrese los números 1 y 2"<<endl;
+    cin >> numero1 >> numero2;
+
+
+    cout<<"Los números 1 y 2 son: "<<numero1<<" y "<<numero2;*/
+
+    string nombre;
+    string nombre2;
+    string apellido;
+    string apellido2;
+
+    cout<<"Ingrese su nombre y apellido"<<endl;
+    cin>>nombre>>nombre2>>apellido>>apellido2;
+
+    cout<<"Mucho gusto en conocerte "<<nombre<<" "<<nombre2<<" "<<apellido<<" "<<apellido2;
+}
